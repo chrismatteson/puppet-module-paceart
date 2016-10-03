99056c337d7eabe4a4d508fe3f4a35674694da53
Chris Matteson <chris.matteson@gmail.com>
2016-10-03 16:14:31 +0000

    fixed parameter issue

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,7 +1,7 @@
 class paceart (
   $mount_iso=true,
   $iso_drive='Q',
-  $source = 'http://care.dlservice.microsoft.com/dl/download/E/A/E/EAE6F7FC-767A-4038-A954-49B8B05D04EB/ExpressAndTools%2064BIT/SQLEXPRWT_x64_ENU.exe',
+  $dbsource = 'http://care.dlservice.microsoft.com/dl/download/E/A/E/EAE6F7FC-767A-4038-A954-49B8B05D04EB/ExpressAndTools%2064BIT/SQLEXPRWT_x64_ENU.exe',
   $stagingowner='BUILTIN\Administrators',
   $admin_user = 'vagrant',
   $sa_pass = 'Secure_Pass!',
@@ -12,7 +12,7 @@ class paceart (
   class { 'paceart::prereqs':
     mount_iso    => $mount_iso,
     iso_drive    => $iso_drive,
-    source       => $source,
+    dbsource       => $dbsource,
     stagingowner => $stagingowner,
     admin_user   => $admin_user,
     sa_pass => $sa_pass,
